35485afb9b75b6d1c5e7b68652819f7ae9ce04fa
sb17057 <sbarnes@vermeer.com>
2023-08-25 08:48:01 -0500

    progress bar and blinking ending

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TimerModule } from './timer/timer.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, TimerModule],
+  imports: [BrowserModule, TimerModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/timer/timer-button/timer-button.component.ts
+++ src/app/timer/timer-button/timer-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+// import { faCheck, faX } from '@fortawesome/free-solid-svg-icon';
 
 @Component({
   selector: 'app-timer-button',
@@ -7,13 +8,20 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TimerButtonComponent implements OnInit {
   @Input() text: string;
-  @Input() time: number;
+  @Input() time: number = 0;
+  defaultTime: number;
   timeDisplay: string = '';
   textDisplay: string = '';
+  timerRunning: boolean = false;
+  interval: string | number | NodeJS.Timeout;
+  timerComplete: boolean = false;
+  // faCheck = faCheck;
+  // fax = faX;
 
   ngOnInit() {
     this.textDisplay = this.text;
     this.checkTextLength();
+    this.defaultTime = this.time + 0;
   }
 
   checkTextLength() {
@@ -22,5 +30,53 @@ export class TimerButtonComponent implements OnInit {
     } else {
       this.textDisplay = this.text;
     }
+  }
+
+  progressBarStyle(): string {
+    let width = 8;
+    let ratio = this.time / this.defaultTime;
+    return `width:${width * ratio}rem`;
+  }
+
+  timer() {
+    if (this.timerComplete) {
+      this.timerComplete = false;
+    } else if (this.timerRunning) {
+      this.endTimer();
+    } else {
+      this.startTimer();
+    }
+  }
+
+  startTimer() {
+    this.timerRunning = true;
+    this.timeDisplay = this.transform(this.time);
+    this.checkTextLength();
+    this.interval = setInterval(() => {
+      if (this.time === 0) {
+        this.time = this.defaultTime;
+        this.timerComplete = true;
+        this.endTimer();
+      } else {
+        this.time--;
+        this.timeDisplay = this.transform(this.time);
+      }
+    }, 1000);
+  }
+  transform(value: number): string {
+    const minutes: number = Math.floor(value / 60);
+    return (
+      minutes +
+      ':' +
+      (value - minutes * 60 < 10
+        ? '0' + (value - minutes * 60)
+        : value - minutes * 60)
+    );
+  }
+  endTimer() {
+    this.timerRunning = false;
+    clearInterval(this.interval);
+    this.timeDisplay = '';
+    this.checkTextLength();
   }
 }
