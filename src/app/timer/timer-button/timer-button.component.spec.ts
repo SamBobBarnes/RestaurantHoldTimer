dfa9fa8559f4105624be85ed2845332b86a8d057
sb17057 <sbarnes@vermeer.com>
2023-08-26 16:38:34 -0500

    some timer tests

--- src/app/timer/timer-button/timer-button.component.spec.ts
+++ src/app/timer/timer-button/timer-button.component.spec.ts
@@ -1,22 +1,112 @@
 import { TimerButtonComponent } from './timer-button.component';
 import { renderRootComponent } from '../../common-components/RenderRootComponent';
-import { screen } from '@testing-library/angular';
+import { fireEvent, screen } from '@testing-library/angular';
 
 describe('TimerButtonComponent', () => {
-  beforeEach(() => {});
+  let options: TimerOptions;
+  beforeEach(() => {
+    options = {
+      text: '',
+      time: 0,
+      timerComplete: false,
+      timerRunning: false,
+      displayChoice: false,
+    };
+  });
 
   it('should show text', async () => {
-    await render('test text', 60, '');
+    options.text = 'test text';
+    await render(options);
     expect(screen.getByText('test text')).toBeTruthy();
+  });
+
+  it('should show timer-click div by default', async () => {
+    await render(options);
+    expect(screen.getByTitle('timer-click')).toBeTruthy();
+  });
+
+  it('should show not show timer-click when display choice is true', async () => {
+    options.displayChoice = true;
+    await render(options);
+    expect(screen.queryByTitle('timer-click')).toBeFalsy();
+  });
+
+  it('should start timer when timer-click is clicked', async () => {
+    options.time = 69;
+    await render(options);
+    const timer = screen.getByTitle('timer-click');
+    fireEvent.click(timer);
+    expect(screen.getByTitle('progress-bar')).toBeTruthy();
+  });
+
+  describe('choice', () => {
+    it('should not show choice when timer is running', async () => {
+      options.time = 69;
+      await render(options);
+      const timer = screen.getByTitle('timer-click');
+      fireEvent.click(timer);
+      expect(screen.queryByTitle('choice')).toBeFalsy();
+    });
+
+    it('should show choice when timer is clicked after starting', async () => {
+      options.time = 69;
+      await render(options);
+      const timer = screen.getByTitle('timer-click');
+      fireEvent.click(timer);
+      fireEvent.click(timer);
+      expect(screen.getByTitle('choice')).toBeTruthy();
+    });
+
+    it('should hide choice when cancel is selected without stopping timer', async () => {
+      options.time = 69;
+      await render(options);
+      const timer = screen.getByTitle('timer-click');
+      fireEvent.click(timer);
+      fireEvent.click(timer);
+      const cancel = screen.getByTitle('x');
+      fireEvent.click(cancel);
+      expect(screen.queryByTitle('choice')).toBeFalsy();
+      expect(screen.getByTitle('progress-bar')).toBeTruthy();
+    });
+
+    it('should cancel timer when confirm is selected', async () => {
+      options.time = 69;
+      await render(options);
+      const timer = screen.getByTitle('timer-click');
+      fireEvent.click(timer);
+      fireEvent.click(timer);
+      const confirm = screen.getByTitle('confirm');
+      fireEvent.click(confirm);
+      expect(screen.queryByTitle('choice')).toBeFalsy();
+      expect(screen.queryByTitle('progress-bar')).toBeFalsy();
+    });
+  });
+
+  describe('initial assignments', () => {
+    it('should set defaultTime', async () => {
+      options.time = 69;
+      const { fixture } = await render(options);
+      expect(fixture.componentInstance.defaultTime).toBe(69);
+    });
+
+    it('should set textDisplay', async () => {
+      options.text = 'test text';
+      const { fixture } = await render(options);
+      expect(fixture.componentInstance.textDisplay).toBe('test text');
+    });
   });
 });
 
-async function render(text: string, time: number, timeDisplay: string) {
+interface TimerOptions {
+  text: string;
+  time: number;
+  timerRunning: boolean;
+  timerComplete: boolean;
+  displayChoice: boolean;
+}
+
+async function render(options: TimerOptions) {
   return await renderRootComponent(TimerButtonComponent, {
-    componentProperties: {
-      text,
-      time,
-      timeDisplay,
-    },
+    componentProperties: options,
   });
 }
